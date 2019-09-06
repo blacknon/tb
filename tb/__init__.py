2772b2dd57bb73068f8dc8f047521a84e2dd46f8
blacknon <blacknon@thinkpad25>
2019-09-06 20:30:46 +0900

    add github markdown

--- tb/__init__.py
+++ tb/__init__.py
@@ -1,40 +1,42 @@
 # -*- coding: utf-8 -*-
-## import
+# import
 import sys
 import re
 import select
 import argparse
 from tabulate import tabulate
 
-## def
+
+# def
 def is_stdin():
-    if select.select([sys.stdin,],[],[],0.0)[0]:
+    if select.select([sys.stdin, ], [], [], 0.0)[0]:
         return True
     return False
 
+
 # to table format
-def to_tbl(lines,args):
+def to_tbl(lines, args):
     tb_separator = args.separator + '+'
     tb_header = args.header
     tb_format = args.format
 
-    if args.article == False:
+    if args.article is False:
         tb_list = []
         for line in lines:
             line = line.rstrip()
-            tb_list.append(re.split(tb_separator,line))
-        print(tabulate(tb_list,tablefmt=tb_format,headers=tb_header))
+            tb_list.append(re.split(tb_separator, line))
+        print(tabulate(tb_list, tablefmt=tb_format, headers=tb_header))
     else:
         tb_list = []
         i = -1
         b_lenght = 0
         for line in lines:
             line = line.rstrip()
-            line_array = re.split(tb_separator,line)
+            line_array = re.split(tb_separator, line)
             length = len(line_array)
             if b_lenght != length or length < 2:
-                i=i+1
-                tb_list.insert(i,[])
+                i = i + 1
+                tb_list.insert(i, [])
             tb_list[i].append(line_array)
             b_lenght = len(line_array)
 
@@ -48,23 +50,35 @@ def to_tbl(lines,args):
                 print(string.rstrip())
                 x += 1
             else:
-                print(tabulate(e,tablefmt=tb_format,headers=tb_header))
+                print(tabulate(e, tablefmt=tb_format, headers=tb_header))
                 x += len(e) + 1
+
 
 def main():
     parser = argparse.ArgumentParser(description='Table making from list.')
-    parser.add_argument('-s', '--separator', default=' ', type=str, help='Specify a set of characters to be used to delimit columns.')
-    parser.add_argument('-l', '--header', default='', choices=['keys','firstrow'], help='table header')
-    parser.add_argument('-f', '--format', default='orgtbl', choices=['simple','orgtbl','plain','grid','fancy-grid','pipe','jira','mediawiki','html','latex'],type=str, help='table format.')
-    parser.add_argument('--article', dest='article', default=False, action='store_true', help='article flag')
+    parser.add_argument('-s', '--separator', default=' ', type=str,
+                        help='Specify a set of characters to be used to delimit columns.')
+    parser.add_argument('-l', '--header', default='',
+                        choices=['keys', 'firstrow'], help='table header')
+    parser.add_argument('-f', '--format', default='orgtbl',
+                        choices=[
+                            'simple', 'orgtbl', 'github', 'plain', 'grid',
+                            'fancy-grid', 'pipe', 'jira', 'mediawiki',
+                            'html', 'latex'
+                        ],
+                        type=str, help='table format.')
+    parser.add_argument('--article', dest='article',
+                        default=False, action='store_true',
+                        help='article flag')
     if is_stdin():
         args = parser.parse_args()
         lines = sys.stdin.readlines()
     else:
         parser.add_argument('file_path', action='store', type=str, help='file')
         args = parser.parse_args()
-        lines = open(args.file_path,'r').readlines()
-    to_tbl(lines,args)
-    
+        lines = open(args.file_path, 'r').readlines()
+    to_tbl(lines, args)
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
